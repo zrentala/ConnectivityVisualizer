282a76b0b0d3d72f489d0534eef607a02e6611ed
zrentala <zrentala@gmail.com>
2025-12-11 23:10:34 -0500

    another

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-# app.py
+# # app.py
+# from __future__ import annotations
+# import numpy as np
+# import pandas as pd
+# import pyvista as pv
+# from dash import Dash
+# import dash_bootstrap_components as dbc
+# from interaction.callbacks import register_callbacks
+# from visualization.ui import create_layout
+
+# from utils.global_app_state import GlobalAppState
+# from analysis.threshold import Threshold
+# global_state = GlobalAppState()
+
+# app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG], suppress_callback_exceptions=True)
+# app.layout = create_layout()
+
+
+# # Register callbacks
+# register_callbacks(app, global_state)
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
 from __future__ import annotations
 import numpy as np
 import pandas as pd
@@ -10,22 +33,21 @@ from visualization.ui import create_layout
 
 from utils.global_app_state import GlobalAppState
 from analysis.threshold import Threshold
-        
 
-def create_app(global_state: GlobalAppState) -> Dash:  
-    """Create and initialize the Dash application."""
-    app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG], suppress_callback_exceptions=True)
-    app.layout = create_layout()
-    
+global_state = GlobalAppState()
 
-    # Register callbacks
-    register_callbacks(app, global_state)
+app = Dash(
+    __name__,
+    external_stylesheets=[dbc.themes.CYBORG],
+    suppress_callback_exceptions=True
+)
 
-    return app
+server = app.server  # <-- expose Flask server explicitly
 
+app.layout = create_layout()
+
+# Register callbacks
+register_callbacks(app, global_state)
 
 if __name__ == "__main__":
-    global_state = GlobalAppState()
-    app = create_app(global_state)
-    app.run(debug=True)
-
+    app.run(debug=False)
